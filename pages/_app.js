7dabce2376224acddfce2cbb54298e2c9c76b535
Dan Ott <dan@dtott.com>
2021-09-10 13:01:23 -0400

    update head

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { SessionProvider } from 'next-auth/react'
 import 'tailwindcss/tailwind.css'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import Head from 'next/head'
 
 // Create a client
 const queryClient = new QueryClient()
@@ -9,10 +10,18 @@ const queryClient = new QueryClient()
 // `useSession()` anywhere in your application to access the `session` object.
 export default function App({ Component, pageProps }) {
 	return (
-		<SessionProvider session={pageProps.session}>
-			<QueryClientProvider client={queryClient}>
-				<Component {...pageProps} />
-			</QueryClientProvider>
-		</SessionProvider>
+		<>
+			<Head>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover"
+				/>
+			</Head>
+			<SessionProvider session={pageProps.session}>
+				<QueryClientProvider client={queryClient}>
+					<Component {...pageProps} />
+				</QueryClientProvider>
+			</SessionProvider>
+		</>
 	)
 }
--- pages/_document.js
+++ pages/_document.js
@@ -11,10 +11,6 @@ class MyDocument extends Document {
 			<Html lang="en">
 				<Head>
 					<meta charset="UTF-8" />
-					<meta
-						name="viewport"
-						content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover"
-					/>
 				</Head>
 				<body>
 					<Main />
