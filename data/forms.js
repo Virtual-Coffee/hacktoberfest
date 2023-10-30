b7cd75d94be88b0014b5d35b1854aae7903749d5
BekahHW <34313413+BekahHW@users.noreply.github.com>
2023-10-30 18:29:22 -0400

    Update label

--- data/forms.js
+++ data/forms.js
@@ -260,7 +260,7 @@ export const nonPrContributions = [
 		name: 'ContributionDescription',
 		type: 'Long text',
 		label:
-			'Tell us about your contribution. If you made multiple contributions, please submit a separate form for each one so they get counted!',
+			'Tell us about your contribution. If you made multiple contributions, please submit a separate form for each one so they get counted! Examples of meaningful, non-code contributions: content that supports or helps an open source project, a substantial PR review, triaging 3 or more issues, facilitating an open source event, writing a detailed issue.',
 		help: 'Required',
 		required: true,
 	},
