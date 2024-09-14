f1de1a8076fa9e623fe7bb79c6de0a8994970991
Ayu Adiati <retno.ayu98@gmail.com>
2024-09-14 21:21:06 +0200

    chore: update the year at globals.js

--- util/globals.js
+++ util/globals.js
@@ -2,7 +2,7 @@ import { useMemo } from 'react'
 
 export function useNewSubmissionsClosed() {
 	return useMemo(() => {
-		const submissionsClosed = new Date(2023, 10, 2)
+		const submissionsClosed = new Date(2024, 10, 2)
 		return Date.now() > submissionsClosed.getTime()
 	}, [])
 }
