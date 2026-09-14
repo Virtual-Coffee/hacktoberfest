c9c985268b6e908f960100c17b8814c71fa57328
Dominic Duffin <dominicduffin1@users.noreply.github.com>
2026-09-14 19:13:52 +0100

    Add HCLB Communities nav link (#67)
    Co-authored-by: Ayu Adiati <45172775+adiati98@users.noreply.github.com>

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -53,6 +53,7 @@ const navigation: NavItem[] = [
 	{ name: 'OSWeave', href: '/' },
 	{ name: 'FAQ', href: '/#questions' },
 	{ name: 'Virtual Coffee', href: 'https://virtualcoffee.io' },
+	{ name: 'HCLB Communities', href: 'https://www.hclbcommunities.com' },
 	{ name: 'Code of Conduct', href: 'https://virtualcoffee.io/code-of-conduct' },
 	{ name: 'Dashboard', href: '/dashboard', authOnly: true },
 	{ name: 'Admin', href: '/admin', adminOnly: true, matchPrefix: true },
